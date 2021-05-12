17710b6f0d14a313125c57856132f3c73dcd62a6
Bohnertgl <gary.bohnert@users.noreply.github.com>
2021-05-12 11:12:22 -0400

    updated

--- BikeRentalAgency/Models/ViewModels/RentalsListViewModel.cs
+++ BikeRentalAgency/Models/ViewModels/RentalsListViewModel.cs
@@ -9,6 +9,9 @@ namespace BikeRentalAgency.Models.ViewModels
     public class RentalsListViewModel
     {
         public IList<Bike> Bikes { get; set; } = new List<Bike>();
+        public IList<Type> Types { get; set; } = new List<Type>();
+        public IList<RentalRate> Rates { get; set; } = new List<RentalRate>();
+        public IList<SpecialFeature> Features { get; set; } = new List<SpecialFeature>();
         public PagingInfo PagingInfo { get; set; }
         public string CurrentCategory { get; set; }
     }
