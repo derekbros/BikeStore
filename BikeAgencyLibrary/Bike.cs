7940a1a78f69bb9f11230eeb7616450f94612819
TonyQ2862 <questea1@my.erau.edu>
2021-05-12 23:35:28 -0400

    no message

--- BikeAgencyLibrary/Bike.cs
+++ BikeAgencyLibrary/Bike.cs
@@ -38,3 +38,5 @@ namespace BikeAgencyLibrary
 
         [NotMapped]
         public virtual BikeType Type { get; set; }
+    }
+}
--- BikeAgencyLibrary/Shop.cs
+++ BikeAgencyLibrary/Shop.cs
@@ -20,7 +20,7 @@ namespace BikeAgencyLibrary
         public string AddressLine2 { get; set; }
 
         public virtual BikesInShop BikesInShop { get; set; }
-        public virtual ICollection<Employee> Employees { get; set; }.
+        public virtual ICollection<Employee> Employees { get; set; }
         
           public IEnumerable<Bike> Bikes { get; set; }
     }
